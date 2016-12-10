43684dca4ecdbdae8f2415dcdb73ed9f6f42d622
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2016-12-10 12:14:53 +0530

    Create validnum2.C

---
+++ validnum2.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	printf("%d\n",n);
+	if((n>=1)&&(n<=9))
+	    printf("Enter number is valid one");
+	else
+	printf("you entered number  in range 1 to 9");
+	return 0;
+}
+
